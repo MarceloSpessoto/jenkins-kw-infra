c9c6c60af9f551133f354d83fcba29b8e5d880f7
Marcelo Mendes Spessoto Junior <marcelomspessoto@gmail.com>
2024-06-05 10:17:44 -0300

    Addition of multiple pipelines
    Signed-off-by: Marcelo Mendes Spessoto Junior <marcelomspessoto@gmail.com>

--- jenkins-controller/configuration/jobs/teste.groovy
+++ jenkins-controller/configuration/jobs/teste.groovy
@@ -1,10 +1,23 @@
-multibranchPipelineJob('kworkflow'){
-  branchSources {
-    github {
-      id('1952385')
-      scanCredentialsId('github-app')
-      repoOwner('MarceloSpessoto')
-      repository('kworkflow')
+pipelines = ["codecov", "formatter-shfmt", "integration_tests", "setup_and_doc", "shellcheck-reviewdog", "unit_tests"]
+pipelines.each { pipeline ->
+  create_pipeline(pipeline)
+}
+
+def create_pipeline(String name){
+  multibranchPipelineJob(name){
+    branchSources {
+      github {
+        id('1952385')
+        scanCredentialsId('github-app')
+        repoOwner('MarceloSpessoto')
+        repository('kworkflow')
+      }
     }
+    factory {
+      workflowBranchProjectFactory {
+        scriptPath("jenkinsfiles/${name}")
+      }
+    }
+    displayName(name)
   }
 }
